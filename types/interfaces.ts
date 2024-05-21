3bc46bd58a2170d4140a3948d01b0de43d0f23a2
Nasir Khalid <nasir.khalid.new@gmail.com>
2024-05-22 01:38:05 +0500

    added interfaces for user and jwt

---
+++ types/interfaces.ts
@@ -0,0 +1,45 @@
+import { ReactNode } from "react";
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface JWT {
+  expiry: number;
+  role: string;
+  user_id: string;
+  name?: string;
+}
+
+export enum REDUCER_ACTION_TYPE {
+  SET_USER = "SET_USER",
+  SET_JWT = "SET_JWT",
+}
+
+export type SetJWTAction = {
+  type: REDUCER_ACTION_TYPE.SET_JWT | REDUCER_ACTION_TYPE.SET_USER;
+  payload: JWT;
+};
+
+export type SetUser = {
+  type: REDUCER_ACTION_TYPE.SET_USER;
+  payload: User;
+};
+
+export type ReducerState = {
+  user?: User;
+  jwt: JWT;
+};
+
+export type ReducerAction = SetJWTAction | SetUser;
+
+export interface IContext {
+  state?: ReducerState;
+  dispatch?: React.Dispatch<ReducerAction>;
+}
+
+export interface IProps {
+  children: ReactNode;
+}
