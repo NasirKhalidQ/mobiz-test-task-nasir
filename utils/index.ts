af7f24cb5be5c4de82eec5863c4dfc6b00373f08
Nasir Khalid <nasir.khalid.new@gmail.com>
2024-05-22 01:31:30 +0500

    added method to append jwt to axios instance header

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
 import { AxiosError } from "axios";
 import { deleteCookie } from "cookies-next";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { axiosInstance } from "@/axios";
+import { setCookie } from "cookies-next";
 
 export function handleSessionError(
   error: AxiosError | any,
@@ -16,3 +18,29 @@ export function handleSessionError(
 }
 
 export function handleSuccess(description: string): void {}
+
+export const saveBearer = (bearer: string, name: string) => {
+  setCookie("auth", bearer);
+  setCookie("name", name);
+  axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + bearer;
+};
+
+export const parseJwt = (token: string) => {
+  var base64Url = token.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  return JSON.parse(jsonPayload);
+};
+
+export const parseJwtServer = (token: string) => {
+  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+};
