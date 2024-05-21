542ecb98fc9b0b6b38478a5ce5ae7aa7f3c47b32
Nasir Khalid <nasir.khalid.new@gmail.com>
2024-05-22 02:39:50 +0500

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { deleteCookie } from "cookies-next";
+import { LogOut } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+export const Navbar = () => {
+  const router = useRouter();
+  const logout = () => {
+    deleteCookie("auth");
+    router.push("/login");
+  };
+  const pathname = usePathname();
+
+  const items = [
+    {
+      label: "Dashboard",
+      link: "/dashboard",
+    },
+    {
+      label: "Products",
+      link: "/products",
+    },
+  ];
+
+  return (
+    <nav className="px-16 py-5 bg-[#1C2536] flex justify-between">
+      <div className="flex gap-16 items-center ">
+        <div className="flex gap-3 items-center">
+          <Image
+            src="https://mobizinc.com/wp-content/uploads/2022/06/logo-xl.png"
+            width={38}
+            height={38}
+            alt="mobiz-logo"
+          />
+          <h1 className="text-3xl text-white">Test Task</h1>
+        </div>
+        <ul className="flex gap-3">
+          {items.map((item) => (
+            <li className="flex" key={item.link}>
+              <Link
+                className={`${
+                  item.link === pathname ? "text-white bg-[#ffffff1a]" : ""
+                } py-4 px-8 hover:bg-[#ffffff1a] rounded-xl text-[#a2a6aa] hover:text-white align-middle`}
+                href={item.link}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex gap-4 items-center">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="cursor-pointer" asChild>
+            <Image src="/icons/avatar.svg" alt="user" width={48} height={48} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-24 -ml-20">
+            <DropdownMenuItem className="justify-around" onClick={logout}>
+              <LogOut className="w-5" />
+              <span>Log out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
+  );
+};
