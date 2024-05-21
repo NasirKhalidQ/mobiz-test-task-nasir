9f2a8bcbf5f89710a94db0d82e1ee1ae1fdabff6
Nasir Khalid <nasir.khalid.new@gmail.com>
2024-05-22 02:39:31 +0500

    added mobiz host to nextjs config for image

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BASE_URL: "https://dummyjson.com/",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mobizinc.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
